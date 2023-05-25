650ea8681774e97314068c2c716a385f94b331c3
Rita <dariusraisutis@gmail.com>
2023-05-25 22:43:42 +0300

    checkout component implementation

--- express-app/client/src/components/Checkout.tsx
+++ express-app/client/src/components/Checkout.tsx
@@ -1,39 +1,105 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import Checkbox from "@mui/material/Checkbox";
 import Container from "@mui/material/Container";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
 import Stepper from "@mui/material/Stepper";
+import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
+import AddressForm from "./AddressForm";
+import PaymentForm from "./PaymentForm";
+import ReviewForm from "./ReviewForm";
+import { IProduct } from "./ProductContainer";
+
+
 
 const getContent = (step: number): JSX.Element => {
-    switch(step){
-        case 0: return <></>;
-        case 1: return <></>;
-        case 2: return <></>;
+    switch (step) {
+        case 0: return <AddressForm />;
+        case 1: return <PaymentForm />;
+        case 2: return <ReviewForm />;
         default: throw new Error('Unexpected checkout step');
     }
 }
 
+interface IDeliveryDetails {
+    firstName: string;
+    secondName: string;
+    addressline: string;
+    city: string;
+    region: string;
+    zipCode: string;
+    country: string;
+}
+
+interface IPaymentDetails {
+    nameOnCard: string;
+    cardNumber: number;
+    expiryDate: Date;
+    cvv: number;
+}
+
+interface ICheckoutDetails {
+    products: IProduct[];
+    deliveryDetails: IDeliveryDetails;
+    paymentDetails: IPaymentDetails;
+
+}
+
 const Checkout = (): JSX.Element => {
+    const props: ICheckoutDetails = {
+        products: [
+            {
+                _id: '',
+                title: '',
+                description: '',
+                category: '',
+                price: 0,
+                brand: '',
+                image: '',
+                inStock: false,
+                stock: 0,
+                discount: 0
+            }
+        ],
+        deliveryDetails: {
+            firstName: '',
+            secondName: '',
+            addressline: '',
+            city: '',
+            region: '',
+            zipCode: '',
+            country: '',
+        },
+        paymentDetails: {
+            nameOnCard: '',
+            cardNumber: 0,
+            expiryDate: new Date(),
+            cvv: 0
+        }
+    };
 
     const { state } = useLocation();
     const { productId, quantity, total } = state;
     const [activeStep, setActiveStep] = useState(0);
+    const [checkoutDetails, setCheckoutDetails] = useState<ICheckoutDetails>(props);
     const steps = ['Shipping address', 'Payment details', 'Review your order'];
     const handleNext = () => {
         setActiveStep(activeStep + 1);
     }
 
     const handleBack = () => {
-        setActiveStep(activeStep -1);
+        setActiveStep(activeStep - 1);
     }
     return <>
-        <Container maxWidth="md">
-            <Paper variant="outlined">
+        <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+            <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
                 <Typography component="h1" variant="h4" align="center">
                     Checkout
                 </Typography>
